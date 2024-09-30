669822e3e0e35794d88d1a16ba9d99b4d335159f
nikolas.rieger <nikolas.rieger@student.hpi.de>
2024-09-30 17:28:08 +0200

    feat: add emotion generation

--- src/app.py
+++ src/app.py
@@ -6,6 +6,9 @@ from flask_cors import CORS
 import base64
 import json
 import time
+from model import Model
+from dotenv import load_dotenv
+from os import getenv
 
 app = Flask(__name__)
 CORS(app)
@@ -45,16 +48,19 @@ def generate_lip_sync():
             Part of the area affected will be beneath the Matterhorn, one of Europe's tallest mountains, and close to a number of popular ski resorts.
             Large sections of the Swiss-Italian border are determined by glacier ridgelines or areas of perpetual snow, but melting glaciers have caused these natural boundaries to shift, leading to both countries seeking to rectify the border."""
 
+        text = model.generate_summary(text)
+
         sentences = [sentence.strip() for sentence in text.split(".") if sentence.strip()]
 
         def generate_lip_sync_stream():
             ticker = 0
             lipsync_generator = LipSync()
-            facial_expression = "neutral"
 
             for sentence in sentences:
                 audio_base64, lipsync_data = lipsync_generator.run_lip_sync(sentence + ".")
-                animation = "Idle" if ticker == 1 else "TalkingTwo"
+                expr = json.loads(model.generate_expression(sentence))
+                animation = expr["animation"]
+                facial_expression = expr["facialExpression"]
                 ticker = 1 - ticker
 
                 response = {
@@ -75,4 +81,6 @@ def generate_lip_sync():
 
 
 if __name__ == "__main__":
+    load_dotenv()
+    model = Model(getenv("GEMINI_API_KEY"))
     app.run(debug=True)
---
+++ src/model.py
@@ -0,0 +1,23 @@
+from google.generativeai import configure, GenerativeModel, GenerationConfig
+
+
+class Model:
+    def __init__(self, api_key: str):
+        configure(api_key=api_key)
+        self.__model = GenerativeModel("gemini-1.5-flash")
+
+    def generate_expression(self, sentence: str):
+        res = self.__model.generate_content(
+            "You are a news reporter. You are reporting on a news story. Do not oversell it." +
+            f"Return a matching facial expression and animation for this sentence: {sentence}." +
+            "You can choose a facial expression from: smile, sad, angry, surprised, funnyFace, and default" + 
+            "and an animation from: Idle, TalkingOne, TalkingTwo, SadIdle, Defeated, Angry, Surprised, DismissingGesture, and ThoughtfulHeadShake." +
+            "Return a JSON in the following format: {'facialExpression': str, 'animation': str}", generation_config=GenerationConfig(response_mime_type="application/json")
+)
+        return res.text
+    
+    def generate_summary(self, article: str):
+        res = self.__model.generate_content(
+            f"Write a summary for this article in the same language. Make it interesting and engaging while keeping the main points: {article}"
+        )
+        return res.text
